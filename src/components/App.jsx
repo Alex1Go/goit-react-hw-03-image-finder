c4ada6d695877b6571a50eea140693c29561b66b
Alex1Go <125027886+Alex1Go@users.noreply.github.com>
2023-08-18 23:29:04 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,21 @@
-export const App = () => {
-  return (
-    <div></div>
-  );
+import { Component } from "react"
+import { Searchbar } from "./Searchbar/Searchbar"
+import { ImageGalerry } from "./ImageGallery/ImageGallery"
+import { Button } from "./Button/Button"
+
+export class App extends Component() {
+  state = {
+    images: [],
+    querry: '',
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Searchbar />
+        <ImageGalerry />
+        <Button/>
+    </div>
+  )};
 };
