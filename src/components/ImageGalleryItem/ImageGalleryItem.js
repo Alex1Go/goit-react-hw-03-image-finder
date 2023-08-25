1082488f7a219719913bd59d47b8e02b0a254fa3
Alex1Go <125027886+Alex1Go@users.noreply.github.com>
2023-08-25 15:48:40 +0300

    add

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -4,50 +4,40 @@ import Modal from 'react-modal';
 
 Modal.setAppElement('#root');
 
-// const customStyles = {
-//   content: {
-//     // zIndex: 1500,
-//     // top: '50%',
-//     // left: '50%',
-//     // right: 'auto',
-//     // bottom: 'auto',
-//     // marginRight: '-50%',
-//     // transform: 'translate(-50%, -50%)',
-//   },
-// };
 export class ImageGalleryItem extends Component {
-      state = {
-        isModalOpen: false,
-    }
-  openModal = () => this.setState({ isModalOpen: true });
+  state = {
+    isModalOpen: false,
+  }
 
-  closeModal = () => this.setState({ isModalOpen: false });
+  modalToggle = () => {
+    this.setState((prevState) => ({
+      isModalOpen: !prevState.isModalOpen,
+    }));
+  };
 
   render() {
-   const { webformatURL, largeImageURL, onClick } = this.props;
+    const { webformatURL, largeImageURL, onClick } = this.props;
+
 
     return (
-    <li className={css.imageGalleryItem}  >
-      <img 
-        src={webformatURL}
+      <li className={css.imageGalleryItem}>
+        <img
+          src={webformatURL}
           alt="foto"
-          onClick={this.openModal}
-        // onClick={() => onClick(largeImageURL)}
-        className={css.imageGalleryItemImage}
+          onClick={this.modalToggle} 
+          className={css.imageGalleryItemImage}
         />
         <Modal
-        isOpen={this.state.isModalOpen}
-        onRequestClose={this.closeModal}
-        // style={customStyles}
-      >
-          {/* <button onClick={this.closeModal}>close</button> */}
-          <div className={css.overlay} onClick={this.closeModal}>
-            <div className={css.modal} >
-             <img onClick={() => onClick(largeImageURL)} src={largeImageURL} alt="foto" />
+          isOpen={this.state.isModalOpen}
+          onRequestClose={this.modalToggle}
+        >
+          <div className={css.overlay}>
+            <div className={css.modal}>
+              <img onClick={() => onClick(largeImageURL)} src={largeImageURL} alt="foto" />
             </div>
           </div>
-      </Modal>
+        </Modal>
       </li>
-  )};
+    );
+  }
 }
-
